fc483dfbc0e5abb3b7fa8399715d56aafa0bb545
oussamalahrizi <flyeroussama@gmail.com>
2025-03-08 06:25:37 +0000

    match making #3

--- api_build/api_service/api_core/urls.py
+++ api_build/api_service/api_core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import GetUserData, GetNotification
+from .views import GetUserData, GetNotification, GetUserService
 
 urlpatterns = [
     path("user/me/", GetUserData.as_view(), name='user-data'),
     path("user/notifications/", GetNotification.as_view(), name='user-data'),
+    path("user/<str:id>/", GetUserService.as_view(), name='user-data-service'),
 ]
