fde878c715d9a5b404051c705483cfe18088c5db
ikhabour <ilyasskhabouri1337sk@gmail.com>
2025-03-23 05:41:01 +0000

    Fixed a few errors

--- match_making/match_service/matchmaking/views.py
+++ match_making/match_service/matchmaking/views.py
@@ -21,45 +21,6 @@ class APIException(Exception):
 
 
 
-class FindMatchTic(APIView):
-    permission_classes = [IsAuthenticated]
-    cache = Queue
-
-    @async_to_sync
-    async def get_status(self, user_id : str):
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.get(f"{API_DATA}{user_id}/")
-                response.raise_for_status()
-                data = response.json()
-                return data
-        except (httpx.ConnectError, httpx.ConnectTimeout, httpx.HTTPError):
-            raise APIException(code=500, detail="Failed to reach API Service")
-        except httpx.HTTPStatusError as e:
-            if e.response.status_code == 404:
-                return None
-            detail = await response.json()
-            raise APIException(code=e.response.status_code, detail=detail)
-        except Exception as e:
-            raise APIException(detail='Internal Server Error')
-    
-    def get(self, request : Request, *args, **kwargs):
-        current_user = {'id' : request.user.id}
-        try:
-            user_data = self.get_status(user_id=current_user["id"])
-            if not user_data:
-                return Response(status=status.HTTP_404_NOT_FOUND,
-                                data={"detail" : "User Not Found."})
-            if user_data["status"] != "online":
-                return Response(status=status.HTTP_400_BAD_REQUEST,
-                                data={"detail" : f'It appears that you are {user_data["status"]}'})
-            self.cache.store_player(current_user['id'], "tic")
-            return Response(data={"detail" : "We are looking for a match : TIC"})
-            
-        except APIException as e:
-            return Response(status=e.code, data={"detail" : e.detail})
-
-
 class FindMatchPong(APIView):
 
     permission_classes = [IsAuthenticated]
@@ -94,7 +55,7 @@ class FindMatchPong(APIView):
                 return Response(status=status.HTTP_400_BAD_REQUEST,
                                 data={"detail" : f'It appears that you are {user_data["status"]}'})
             self.cache.store_player(current_user['id'], "pong")
-            return Response(data={"detail" : "We are looking for a match : PONG"})
+            return Response(data={"detail" : "We are looking for a match."})
             
         except APIException as e:
             return Response(status=e.code, data={"detail" : e.detail})
@@ -136,7 +97,7 @@ class CheckGame(APIView):
             if user_id not in players:
                 return Response(status=status.HTTP_400_BAD_REQUEST,
                                 data={'detail' : 'User Not in the game'})
-            return Response(status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_200_OK, data={"detail" : "OK"})
         except serializers.ValidationError:
             return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
 
