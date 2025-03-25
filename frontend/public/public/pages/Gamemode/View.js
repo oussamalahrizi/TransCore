9c5389e206ecbf7846769aefa2122b241e5ae02d
YaachiRo <yachiro2001@protonmail.com>
2025-03-25 06:58:29 +0000

    some chagnes in frontend

--- frontend/public/public/pages/Gamemode/View.js
+++ frontend/public/public/pages/Gamemode/View.js
@@ -1,15 +1,27 @@
 export default /*html */ `
     <div class="container-gamemode">
-      <div class="card" id="singleplayer-card">
-        <div class="heading">Singleplayer</div>
+      <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; margin-bottom: 1.5rem;">
+          <div class="pang-container">
+            <div class="card" id="singleplayer-card">
+              <div class="heading">Singleplayer</div>
+            </div>
+            <div class="game-title">Ping Pong</div>
+            <div class="card" id="multiplayer-card">
+              <div class="heading">Multiplayer</div>
+            </div>
+          </div>
       </div>
-
-      <div class="card" id="multiplayer-card">
-        <div class="heading">Multiplayer</div>
+      <div style="display: flex; flex-direction: row; align-items: center; justify-content: center;">
+        <div class="xo-container">
+          <div class="card" id="singleplayer-card-xo">
+            <div class="heading">Singleplayer</div>
+          </div>
+          <div class="game-title">Tic Tac Toe</div> 
+          <div class="card" id="multiplayer-card-xo">
+            <div class="heading">Multiplayer</div>
+          </div>
+        </div>
       </div>
-
-      <div class="card" id="tournament-card">
-        <div class="heading">Tournament</div>
-      </div>
+      
     </div>
 `;
