2ceb341db8381ea349dc209b1e1bb779b0a7e625
YaachiRo <yachiro2001@protonmail.com>
2025-03-25 08:15:38 +0000

    stylish game mode page

--- frontend/public/public/pages/Gamemode/View.js
+++ frontend/public/public/pages/Gamemode/View.js
@@ -3,10 +3,12 @@ export default /*html */ `
       <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; margin-bottom: 1.5rem;">
           <div class="pang-container">
             <div class="card" id="singleplayer-card">
+            <div class="title">Ping Pong</div>
               <div class="heading">Singleplayer</div>
             </div>
             <div class="game-title">Ping Pong</div>
             <div class="card" id="multiplayer-card">
+            <div class="title">Ping Pong</div>
               <div class="heading">Multiplayer</div>
             </div>
           </div>
@@ -14,10 +16,12 @@ export default /*html */ `
       <div style="display: flex; flex-direction: row; align-items: center; justify-content: center;">
         <div class="xo-container">
           <div class="card" id="singleplayer-card-xo">
+            <div class="title">Tic Tac Toe</div>
             <div class="heading">Singleplayer</div>
           </div>
           <div class="game-title">Tic Tac Toe</div> 
           <div class="card" id="multiplayer-card-xo">
+            <div class="title">Tic Tac Toe</div>
             <div class="heading">Multiplayer</div>
           </div>
         </div>
