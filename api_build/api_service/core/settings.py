05779b958e1ecd8d8a75dc91e5f4c585e87f2afd
Nisrine El Harkani <nel-hark@c4r9p9.1337.ma>
2025-03-28 00:13:36 +0000

    test

--- api_build/api_service/core/settings.py
+++ api_build/api_service/core/settings.py
@@ -84,16 +84,16 @@ ASGI_APPLICATION = 'core.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', ''),
-        'USER': os.getenv('POSTGRES_USER', ''),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', '')
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('POSTGRES_DB', ''),
+#         'USER': os.getenv('POSTGRES_USER', ''),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+#         'HOST': os.getenv('DB_HOST', ''),
+#         'PORT': os.getenv('DB_PORT', '')
+#     }
+# }
 
 
 # Password validation
